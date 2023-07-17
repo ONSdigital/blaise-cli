63fe03783039b2c02f63a31448ad42717d2a8fa2
elthorne <elinor.thorne@ons.gov.uk>
2023-07-17 12:45:27 +0100

    wip: revert
    refs: BLAIS5-3772

--- blaise.cli/Blaise.Cli/Program.cs
+++ blaise.cli/Blaise.Cli/Program.cs
@@ -1,10 +1,9 @@
-﻿using Blaise.Cli.Core.Interfaces;
+﻿﻿using System;
+using Blaise.Cli.Core.Interfaces;
 using Blaise.Cli.Core.Services;
 using Blaise.Nuget.Api.Api;
 using Blaise.Nuget.Api.Contracts.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.IO;
 
 namespace Blaise.Cli
 {
@@ -12,57 +11,20 @@ namespace Blaise.Cli
     {
         private static void Main(string[] args)
         {
-            try
-            {
-                Console.WriteLine("Blaise ClI");
-                Console.WriteLine("");
+            var serviceProvider = new ServiceCollection()
+            .AddSingleton<ICommandService, CommandService>()
+            .AddTransient<IBlaiseFileService, BlaiseFileService>()
+            .AddTransient<IBlaiseFileApi, BlaiseFileApi>()
+            .BuildServiceProvider();
 
-                var serviceProvider = new ServiceCollection()
-                    .AddSingleton<ICommandService, CommandService>()
-                    .AddTransient<IBlaiseFileService, BlaiseFileService>()
-                    .AddTransient<IBlaiseFileApi, BlaiseFileApi>()
-                    .BuildServiceProvider();
+            var commandService = serviceProvider.GetService<ICommandService>();
 
-                var commandService = serviceProvider.GetService<ICommandService>();
+            if (commandService == null)
+            {
+                throw new ApplicationException("There was an error in creating the command parser");
+            }
 
-                if (commandService == null)
-                {
-                    throw new ApplicationException("There was an error in creating the command parser");
-                }
-
-                commandService.ParseArguments(args);
-            }
-            catch (ArgumentException e)
-            {
-#if DEBUG
-                Console.WriteLine($"Blaise configuration error: {e.Message}");
-#else
-                throw new ArgumentException($"Blaise configuration error: {e.Message}");
-#endif
-            }
-            catch (FileNotFoundException e)
-            {
-#if DEBUG
-                Console.WriteLine($"File Not Found: {e.Message}");
-#else
-                throw new FileNotFoundException($"File Not Found: {e.Message}");
-#endif
-            }
-            catch (Exception e)
-            {
-#if DEBUG
-                Console.WriteLine($"Error: {e.Message}");
-#else
-                throw new Exception($"Error: {e.Message}");
-#endif
-            }
-            finally
-            {
-#if DEBUG
-                Console.WriteLine("Press any key to exit");
-                Console.ReadKey();
-#endif
-            }
+            commandService.ParseArguments(args);
         }
     }
 }

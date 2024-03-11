e6845fe824fa60e0f865e1982ceb351c50d8f52e
James Williams <james.williams@ons.gov.uk>
2024-03-11 14:52:15 +0000

    feat: add install of CAPI instruments via cli

---
+++ blaise.cli/Blaise.Cli.Core/Interfaces/IBlaiseQuestionnaireService.cs
@@ -0,0 +1,9 @@
+﻿using Blaise.Nuget.Api.Contracts.Enums;
+
+namespace Blaise.Cli.Core.Interfaces
+{
+    public interface IBlaiseQuestionnaireService
+    {
+        void InstallQuestionnaire(string questionnaireName, string serverParkName, string questionnaireFile);
+    }
+}
---
+++ blaise.cli/Blaise.Cli.Core/Models/QuestionnaireOptions.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+namespace Blaise.Cli.Core.Models
+{
+    [Verb("questionnaireinstall", HelpText = "Data delivery pipeline options")]
+    public class QuestionnaireOptions
+    {
+        [Option('q', "questionnaireName", Required = true, HelpText = "The name of the questionnaire")]
+        public string QuestionnaireName { get; set; }
+
+        [Option('s', "serverParkName", Required = true, HelpText = "The name of the server park that houses the questionnaire data")]
+        public string ServerParkName { get; set; }
+
+        [Option('f', "questionnaireFile", Required = true, HelpText = "The package file containing the questionnaire file")]
+        public string File { get; set; }
+    }
+}
